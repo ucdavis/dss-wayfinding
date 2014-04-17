61e26861e721568d8ff7b3e9118f4c044c4505be
Jacob Saporito <jmsaporito@ucdavis.edu>
2014-04-17 08:10:11 -0700

    RSS links should be unclickable.

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    // When flyout button is clicked, hide start screen stuff
     $('.flyoutButton').click(function() {
         $.get($('input', this)[0].value, function(data) {
             $('#content').html(data);
@@ -7,7 +8,11 @@ $(document).ready(function() {
         })
     });
 
+    // On homebutton click, bring back the start screen
     $('#homeButton img').click(function() {
         $('#rss, #image, #navigation').removeClass('offscreen');
     });
+
+    // RSS links should be unclickable
+    $('#rss').on('click', 'a', function(e) {e.preventDefault();});
 });
