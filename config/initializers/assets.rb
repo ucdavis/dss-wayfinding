af60fe5a1d95859396c7863346186a5f1f32b828
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2014-07-01 18:18:27 -0700

    Adding bootstrap

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,5 @@
 Rails.application.config.assets.precompile += %w( jquery.wayfinding.js )
+Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.eot )
+Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.woff )
+Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.svg )
+Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.ttf )
