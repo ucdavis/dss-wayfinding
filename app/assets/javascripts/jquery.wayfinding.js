efac17985dbeed89481b67b6aba27049bfc7cf7f
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2014-07-18 02:41:00 -0700

    Implementing basic floor selector

--- app/assets/javascripts/jquery.wayfinding.js
+++ app/assets/javascripts/jquery.wayfinding.js
@@ -1347,6 +1347,68 @@
 
 	}; // wayfinding function
 
+    $('.floor1-button').click(function(){
+        $('#floor1').css('visibility','visible');
+        $('#floor1').css('display','inline-block');
+        $('#floor2').css('visibility','hidden');
+        $('#floor2').css('display','none');
+        $('#floor3').css('visibility','hidden');
+        $('#floor3').css('display','none');
+        $('#floor4').css('visibility','hidden');
+        $('#floor4').css('display','none');
+        $('#floor5').css('visibility','hidden');
+        $('#floor5').css('display','none');
+    });
+    $('.floor2-button').click(function(){
+        $('#floor1').css('visibility','hidden');
+        $('#floor1').css('display','none');
+        $('#floor2').css('visibility','visible');
+        $('#floor2').css('display','inline-block');
+        $('#floor3').css('visibility','hidden');
+        $('#floor3').css('display','none');
+        $('#floor4').css('visibility','hidden');
+        $('#floor4').css('display','none');
+        $('#floor5').css('visibility','hidden');
+        $('#floor5').css('display','none');
+    });
+    $('.floor3-button').click(function(){
+        $('#floor1').css('visibility','hidden');
+        $('#floor1').css('display','none');
+        $('#floor2').css('visibility','hidden');
+        $('#floor2').css('display','none');
+        $('#floor3').css('visibility','visible');
+        $('#floor3').css('display','inline-block');
+        $('#floor4').css('visibility','hidden');
+        $('#floor4').css('display','none');
+        $('#floor5').css('visibility','hidden');
+        $('#floor5').css('display','none');
+    });
+    $('.floor4-button').click(function(){
+        $('#floor1').css('visibility','hidden');
+        $('#floor1').css('display','none');
+        $('#floor2').css('visibility','hidden');
+        $('#floor2').css('display','none');
+        $('#floor3').css('visibility','hidden');
+        $('#floor3').css('display','none');
+        $('#floor4').css('visibility','visible');
+        $('#floor4').css('display','inline-block');
+        $('#floor5').css('visibility','hidden');
+        $('#floor5').css('display','none');
+    });
+    $('.floor5-button').click(function(){
+        $('#floor1').css('visibility','hidden');
+        $('#floor1').css('display','none');
+        $('#floor2').css('visibility','hidden');
+        $('#floor2').css('display','none');
+        $('#floor3').css('visibility','hidden');
+        $('#floor3').css('display','none');
+        $('#floor4').css('visibility','hidden');
+        $('#floor4').css('display','none');
+        $('#floor5').css('visibility','visible');
+        $('#floor5').css('display','inline-block');
+    });
+
+
 }(jQuery));
 
 //  ]]>
