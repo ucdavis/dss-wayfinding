bca1db6dfc6802a11e95f4d0f1021540eddebe68
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2014-07-24 14:01:38 -0700

    Adding parse logic for rooms and departments from csv, unstable

--- app/controllers/administration_controller.rb
+++ app/controllers/administration_controller.rb
@@ -22,27 +22,66 @@ class AdministrationController < ApplicationController
     csv_path = csv_path.path
 
     CSV.foreach(csv_path, :headers => true) do |row|
-      building = row[1]
-      room = row[3]
-      room_name = row[4]
-      department = row[7]
-      person_name = row[9] #can be general purpose name like 'general graduate student'
-      person_department = row[11]
-      person_organization = row [12]
-      phone = row[13]
-      email = row[14]
 
-      # Found new room?
+      csv_building = row[1]
+      csv_room_number = row[3]
+      csv_room_name = row[4]
+      csv_department = row[7]
+      csv_person_name = row[9] #can be general purpose name like 'general graduate student'
+      csv_last_name, csv_first_name = csv_person_name.split(',').map(&:strip)
 
-      # Found existing room? update entry
+      csv_person_department = row[11]
+      csv_person_organization = row [12] # Use this for department, remove leading org code
+      csv_phone = row[13]
+      csv_email = row[14]
+
+      if csv_room != ""
+
+        # Found new room?
+        results = Room.where(csv_room_number: room_number)
+        if results.length = 0
+          room = Room.new
+          if not csv_room_number.blank?
+            room.room_number = csv_room_number
+          else
+            logger.error "csv_room is missing a room number"
+            # TODO - should the loop for this row be broken out of if room number doesn't exist?, without a room, there is no location
+          end
+        else
+          room = results.first
+        end
+        
+        # Found existing room? update entry
+        # Ensure custom data has not already been set
+        if room.room_name.blank?      
+          room.room_name = csv_room_name
+        end
+        room.save
+      end
 
       # Found new department
+      results = Department.where(title: csv_person_organization)
+      if results.length == 0
+        department = Department.new
+        if not csv_person_organization.blank?
+          department.title = csv_person_organization
+        else
+          logger.info "entry was missing an organization"
+        end
+      else
+        department = results.first
+        department.save
+      end
+
+      # People parsing need to account for multiple rooms associated to an individual, and multiple people with the same first/last name
 
       # Found new person?
-
+#      results_first = Person.where(first: csv_first_name)
+#      results_last = Person.where(last: csv_last_name)
+#      results = (results_first + results_last).uniq
       # Found existing person? update entry
-    end
 
+    end 
     render :nothing => true
   end
 end
