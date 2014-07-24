6e08795abfaede9d32e026bfbd1987a1aa3ef2ff
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2014-07-24 16:32:52 -0700

    Adding person persisting from csv, does not yet persist room/department associations

--- app/controllers/administration_controller.rb
+++ app/controllers/administration_controller.rb
@@ -20,9 +20,11 @@ class AdministrationController < ApplicationController
 
     csv_path = params[:uploaded_csv]
     csv_path = csv_path.path
+    # Wipe database on csv load, will need to be modified when event RSS feeds are fixed
+    Person.destroy_all
+    Department.destroy_all
 
     CSV.foreach(csv_path, :headers => true) do |row|
-
       csv_building = row[1]
       csv_room_number = row[3]
       csv_room_name = row[4]
@@ -30,15 +32,17 @@ class AdministrationController < ApplicationController
       csv_person_name = row[9] #can be general purpose name like 'general graduate student'
       csv_last_name, csv_first_name = csv_person_name.split(',').map(&:strip)
 
-      csv_person_department = row[11]
-      csv_person_organization = row [12] # Use this for department, remove leading org code
+      csv_person_department = row[11] # appears to be less relevant to our needs
+      csv_organization = row[12]
+      csv_org_code, csv_person_organization = csv_organization.split(' ', 2) # Use this for department
       csv_phone = row[13]
       csv_email = row[14]
 
+      # Don't bother with this row if a room number doesn't exist
       if csv_room_number != ""
-
-        # Found new room?
+        # Parse Rooms
         results = Room.where(room_number: csv_room_number)
+        # Found new room?
         if results.empty?
           room = Room.new
           if not csv_room_number.blank?
@@ -59,31 +63,56 @@ class AdministrationController < ApplicationController
         room.save
       end
 
-      # Found new department
-      results = Department.where(title: csv_person_organization)
-      if results.empty?
-        logger.info "creating new department"
-        department = Department.new
-        if csv_person_organization.present?
-          department.title = csv_person_organization
-          department.save
-          logger.info "saving new department"
+      # Parse Department
+      # Don't bother with department/person parsing, something is wrong with this row 
+      unless csv_organization.include?("Identity Purged")
+        results = Department.where(title: csv_person_organization)
+        department = nil
+        if results.empty?
+          logger.info "creating new department"
+          department = Department.new
+          if csv_person_organization.present?
+            department.title = csv_person_organization
+            department.save
+            logger.info "saving new department"
+          else
+            logger.info "entry was missing an organization"
+          end
         else
-          logger.info "entry was missing an organization"
+          logger.info "department already exists"
+          department = results.first
         end
-      else
-        logger.info "department already exists"
-        department = results.first
+
+        results = Person.where(email: csv_email)
+
+        # Found new person?
+        if results.empty?
+          person = Person.new
+
+        # Found existing person?
+        else
+          person = results.first
+        end
+        # Ensure room is associated
+        if not person.rooms.include?(room)
+#          person.rooms << room
+        end
+
+        # Ensure department is associated
+        # Currently assumes each person has one department, seems to be the case from the data
+        if person.department.blank?
+          if department.present?
+            logger.info "associating department"
+#            person.department = department
+          end
+        end
+        person.email = csv_email
+        person.phone = csv_phone
+        person.first = csv_first_name
+        person.last = csv_last_name
+        logger.info "Attempting to save person:" + person.inspect
+        person.save
       end
-
-      # People parsing need to account for multiple rooms associated to an individual, and multiple people with the same first/last name
-
-      # Found new person?
-#      results_first = Person.where(first: csv_first_name)
-#      results_last = Person.where(last: csv_last_name)
-#      results = (results_first + results_last).uniq
-      # Found existing person? update entry
-
     end 
     render :nothing => true
   end
