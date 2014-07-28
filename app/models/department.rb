2227108c00f986b904e815b61c2aa1e2ce2d4e44
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2014-07-28 02:01:59 -0700

    Adding room to department

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,6 +1,8 @@
 class Department < DirectoryObject
   validates :title, uniqueness: true, presence: true
-
+  validates :room_id, presence: false
+  
   has_many :people
   has_many :events
+  belongs_to :room
 end
