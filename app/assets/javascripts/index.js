9bc59a1c432fd62c69a5908a1487c9667c289ef1
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2014-08-04 16:57:51 -0700

    Removing pagination code/styling

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,10 +3,6 @@
 
 $(document).ready(function(){
 
-  // Initial pagination grouping
-//  $('.pag').removeClass('pag-show').addClass('pag-hidden');
-//  $('.paginate-group-'+1).removeClass('pag-hidden').addClass('pag-show');
-
   if ( typeof notice !== 'undefined' && notice ) {
     $(".alert").addClass("alert-success").css('visibility','visible');
     $(".alert span.notice").text(notice);
@@ -49,12 +45,5 @@ $(document).ready(function(){
       $(this).find('input[type="submit"]').addClass('btn-danger')
       $(this).find('.form-group').append('<p class="help-block col-sm-2 status">Error saving...</p>');
     }
-  });
-  
-  $(".paginate-button").click(function(event) {
-//    event.preventDefault();
-//    var pagination_group = $(this).text();
-//    $('.pag').removeClass('pag-show').addClass('pag-hidden');
- //   $('.paginate-group-'+pagination_group).removeClass('pag-hidden').addClass('pag-show');
   });
 });
