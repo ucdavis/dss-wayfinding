fd745289df662d03ec7ef9208545c664aec6a364
Obada Kadri <okadri@ucdavis.edu>
2014-08-04 10:26:36 -0700

    Resize left icons on window.resize

--- app/assets/javascripts/animate.js
+++ app/assets/javascripts/animate.js
@@ -1,14 +1,3 @@
-$(function() {
-  // Ease-in the left nav only if the user is coming from the landing page
-  if (document.referrer == window.location.origin + "/") {
-    $('ul.vertical-nav').css('left','-200%');
-    setTimeout(function(){
-      $('ul.vertical-nav').animate({left: 0}, 500)
-      }, 0);
-  }
-
-});
-
 function resizeIcons() {
   var iconSize = Math.min(window.innerHeight/6-40, 0.1*window.innerWidth);
   $('.vertical-nav li a')
@@ -18,3 +7,17 @@ function resizeIcons() {
     .css('font-size', iconSize/2 + 'px')
     .css('border-width', 0.08*iconSize + 'px');
 }
+
+$(function() {
+  // Ease-in the left nav only if the user is coming from the landing page
+  if (document.referrer == window.location.origin + "/") {
+    $('ul.vertical-nav').css('left','-200%');
+    setTimeout(function(){
+      $('ul.vertical-nav').animate({left: 0}, 500)
+      }, 0);
+  }
+
+  $( window ).resize(function() {
+    resizeIcons();
+  });
+});
