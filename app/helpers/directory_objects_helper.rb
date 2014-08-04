78e112f41eeb710d93058c7f64936455dc99e7a8
Christopher Thielen <cmthielen@ucdavis.edu>
2014-08-04 14:11:26 -0700

    DRYed up usage of jQuery Wayfinding plugin. Fixes bug in show.html.erb where old caches were being used.

---
+++ app/helpers/directory_objects_helper.rb
@@ -0,0 +1,31 @@
+module DirectoryObjectsHelper
+  def wayfinding_plugin(svg_paths, dataStoreCacheUrl, origin)
+    str = "$('#map').wayfinding({
+    'maps': ["
+
+    svg_paths.each_with_index do |svg, i|
+      str = str + "{'path': '" + svg + "', 'id': 'floor" + i.to_s + "'}"
+
+      if i != svg_paths.length
+        str = str + ","
+      end
+    end
+
+    str = str + "],
+    'dataStoreCache': '" + dataStoreCacheUrl + "',
+    'path': {
+      width: 3,
+      color: 'rgb(194, 110, 96)',
+      radius: 8,
+      speed: 8
+    },
+    'startpoint': function () {
+      console.debug('Start location is ', '" + origin + "');
+      return '" + origin + "';
+    }
+  });
+  "
+
+    return str.html_safe
+  end
+end
