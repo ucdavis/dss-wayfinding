7d7168a1fcd0e834ea479ebcfc9ff835a575c5aa
Christopher Thielen <cmthielen@ucdavis.edu>
2014-08-04 17:26:00 -0700

    Handle unset @origin a little more gracefully

--- app/assets/javascripts/floor.js
+++ app/assets/javascripts/floor.js
@@ -3,9 +3,11 @@ $(function() {
   $("a.btn-floor").click(function(event) {
     console.log("detected floor change!");
     event.preventDefault();
+
     // Hide other floors and display the selected one
     $("#map").children('div').css("display", "none");
     $("#" + this.dataset.mapid).css("display", "inline-block");
+
     // Make the clicked button active
     $("a.btn-floor").removeClass('active');
     $(this).addClass('active');
