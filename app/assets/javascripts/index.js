1a92fd7c3f3690a1c3b7280706ec390b8c935973
Lloyd Wheeler <ltwheeler@ucdavis.edu>
2014-11-20 03:21:28 -0800

    For #36, Adding functional scrubber that handles touch events, scales with more or less scrubber categories, and works with scrollspy

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -55,43 +55,66 @@ $(document).ready(function(){
     window.document.location = $(this).data("url");
   });
 
-$( "#slider-vertical" ).slider({
-      orientation: "vertical",
-      range: "min",
-      min: 0,
-      max: 100,
-      value: 60,
-      slide: function( event, ui ) {
-        
-        if (ui.value < 30)
-        {
-            $('#nav-three').addClass('active');
-            $('#nav-two').removeClass('active');
-            $('#nav-one').removeClass('active');
-            element_to_scroll_to = document.getElementById("three");
-            element_to_scroll_to.scrollIntoView();
-        }
-        if (ui.value >=30 && ui.value < 60)
-        {
-            $('#nav-two').addClass('active');
-            $('#nav-three').removeClass('active');
-            $('#nav-one').removeClass('active');
-            element_to_scroll_to = document.getElementById("two");
-            element_to_scroll_to.scrollIntoView();
-        }
-        if (ui.value >=60 && ui.value <= 100)
-        {
-            $('#nav-one').addClass('active');
-            $('#nav-two').removeClass('active');
-            $('#nav-three').removeClass('active');
-            element_to_scroll_to = document.getElementById("one");
-            element_to_scroll_to.scrollIntoView();
-        }
-        $( "#amount" ).val( ui.value );
-      }
-    });
-    $( "#amount" ).val( $( "#slider-vertical" ).slider( "value" ) );
+    // Identify coordinates for scrubber categories
+    var scrubber_categories = $('#slider-vertical li');
+    var scrubber_breakpoints = [];
+    var previous_active_category = "";
 
-//$('.spy-container').scrollspy({ target: '#side-nav' });
+    for(i = 0; i < scrubber_categories.length; i++) {
+        rect = scrubber_categories[i].getBoundingClientRect();
+        scrubber_breakpoints[i] = rect.bottom;
+    }
+
+  // Watches for touch on sidebar scrubber, applies the 'active' class and triggers anchors in response
+  $('#slider-vertical').on("touchmove", function(e){
+    e.preventDefault();
+
+    touch = event.touches.item(0);
+    fingerX = touch.clientX;
+    fingerY = touch.clientY;
+
+    active_category = "";
+
+    // Identify which scrubber category the finger is on
+    for(i = 0; i < scrubber_breakpoints.length; i++) {
+      if(fingerY < scrubber_breakpoints[i]) {
+        active_category = i;
+        i = 100;
+      }
+    }
+
+    // Touch is below the last scrubber_category, so use the last category
+    if(active_category === "") {
+      active_category = (scrubber_breakpoints.length - 1);
+    }
+
+    // Are we on a new category? If so apply styling
+    if ( active_category != previous_active_category ) {
+
+      active_element = $("#scrubber-category_" + active_category);
+      active_element.addClass("active");
+
+      inactive_element = $('#scrubber-category_' + previous_active_category);
+      inactive_element.removeClass("active");
+      
+      // Remove any potential hovering triggered during the movement
+      $("[id^=scrubber-category]").blur();
+      $("[id^=scrubber-category]").children().blur();
+
+      previous_active_category = active_category;
+    }
+
+    // Trigger anchors
+    console.log(active_element.children()[0]);
+    active_element.children()[0].click();
+  });
+
+  $('#slider-vertical').on("touchend", function(e){
+    scrubElement = "";
+  });
+
+  $('#slider-vertical').on("touchstart", function(e){
+    $("[id^=scrubber-category]").removeClass("active");
+  });
 
 });
