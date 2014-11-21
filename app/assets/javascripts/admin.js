771013a09b987c187592d0d7931d135440749c9c
Obada Kadri <okadri@ucdavis.edu>
2014-11-21 10:30:15 -0800

    Split the index.js file to separate the admin page js code

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,88 @@
+//= require redirect
+//= require animate
+
+var ready;
+ready = function() {
+  if ( typeof notice !== 'undefined' && notice ) {
+    $(".alert").addClass("alert-success").css('visibility','visible');
+    $(".alert span.notice").text(notice);
+  } else if ( typeof error !== 'undefined' && error ) {
+    $(".alert").addClass("alert-danger").css('visibility','visible');
+    $(".alert span.notice").text(error);
+  }
+
+  $("#originform").on('ajax:success',function(event, data){
+    if (typeof data.notice !== 'undefined' ) {
+      $(".alert").addClass("alert-success").css('visibility','visible');
+      $(".alert span.notice").text(data.notice);
+    } else if (typeof data.error !== 'undefined' ) {
+      $(".alert").addClass("alert-danger").css('visibility','visible');
+      $(".alert span.notice").text(data.error);
+    }
+    if (typeof data.origin !== 'undefined' ) $("input#origin").val(data.origin);
+
+    setTimeout(function() {
+      $('.alert').fadeOut();
+    }, 2500);
+  }).on('ajax:error',function(xhr, status, error){
+    $(".alert").addClass("alert-danger").show();
+    $(".alert span.notice").text("Error communicating with server!");
+
+    setTimeout(function() {
+      $('.alert').fadeOut();
+    }, 2500);
+  });
+
+  $(".deptform").on('ajax:success',function(event, data){
+    if (typeof data.notice !== 'undefined' ) {
+      // Clear others
+      $('.deptform input[type="submit"]').attr('class','btn btn-default');
+      $('.deptform p.status').hide();
+      // Display status
+      $(this).find('input[type="submit"]').addClass('btn-success')
+      $(this).find('.form-group').append('<p class="help-block col-sm-2 status">Saved successfully...</p>');
+    } else if (typeof data.error !== 'undefined' ) {
+      // Clear others
+      $('.deptform input[type="submit"]').attr('class','btn btn-default');
+      $('.deptform p.status').hide();
+      // Display status
+      $(this).find('input[type="submit"]').addClass('btn-danger')
+      $(this).find('.form-group').append('<p class="help-block col-sm-2 status">Error saving...</p>');
+    }
+  });
+
+  // Build caches progress bar
+  var progress = null;
+  var percantage = '100%';
+  var statsInterval;
+  var getCacheStats = function () {
+    $.get( "/dataStore/stats.json", function( data ) {
+      if ( progress == null ) {
+        // Set interval to keep checking stats
+        statsInterval = setInterval(function(){getCacheStats();}, 10000);
+      }
+
+      if (typeof data == 'undefined' || data.progress == "Completed" ) {
+        // Stop checking if no build in progress
+        progress = "No cache building in porgress"
+        clearInterval(statsInterval);
+      } else {
+        // Update values if process is not completed
+        progress = data.progress;
+        percentage = data.progress;
+      }
+
+      $('#cacheStats').css('width', percentage);
+      $('#cacheStats').text(progress);
+    }).fail(function() {
+      console.error("Could not fetch cache building progress");
+      clearInterval(statsInterval);
+    });
+  }
+
+  getCacheStats();
+
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,67 +3,19 @@
 
 var ready;
 ready = function() {
-  if ( typeof notice !== 'undefined' && notice ) {
-    $(".alert").addClass("alert-success").css('visibility','visible');
-    $(".alert span.notice").text(notice);
-  } else if ( typeof error !== 'undefined' && error ) {
-    $(".alert").addClass("alert-danger").css('visibility','visible');
-    $(".alert span.notice").text(error);
-  }
-
-  $("#originform").on('ajax:success',function(event, data){
-    if (typeof data.notice !== 'undefined' ) {
-      $(".alert").addClass("alert-success").css('visibility','visible');
-      $(".alert span.notice").text(data.notice);
-    } else if (typeof data.error !== 'undefined' ) {
-      $(".alert").addClass("alert-danger").css('visibility','visible');
-      $(".alert span.notice").text(data.error);
-    }
-    if (typeof data.origin !== 'undefined' ) $("input#origin").val(data.origin);
-
-    setTimeout(function() {
-      $('.alert').fadeOut();
-    }, 2500);
-  }).on('ajax:error',function(xhr, status, error){
-    $(".alert").addClass("alert-danger").show();
-    $(".alert span.notice").text("Error communicating with server!");
-
-    setTimeout(function() {
-      $('.alert').fadeOut();
-    }, 2500);
-  });
-
-  $(".deptform").on('ajax:success',function(event, data){
-    if (typeof data.notice !== 'undefined' ) {
-      // Clear others
-      $('.deptform input[type="submit"]').attr('class','btn btn-default');
-      $('.deptform p.status').hide();
-      // Display status
-      $(this).find('input[type="submit"]').addClass('btn-success')
-      $(this).find('.form-group').append('<p class="help-block col-sm-2 status">Saved successfully...</p>');
-    } else if (typeof data.error !== 'undefined' ) {
-      // Clear others
-      $('.deptform input[type="submit"]').attr('class','btn btn-default');
-      $('.deptform p.status').hide();
-      // Display status
-      $(this).find('input[type="submit"]').addClass('btn-danger')
-      $(this).find('.form-group').append('<p class="help-block col-sm-2 status">Error saving...</p>');
-    }
-  });
-
   $(".clickable-row").click(function() {
     window.document.location = $(this).data("url");
   });
 
-    // Identify coordinates for scrubber categories
-    var scrubber_categories = $('#slider-vertical li');
-    var scrubber_breakpoints = [];
-    var previous_active_category = "";
+  // Identify coordinates for scrubber categories
+  var scrubber_categories = $('#slider-vertical li');
+  var scrubber_breakpoints = [];
+  var previous_active_category = "";
 
-    for(i = 0; i < scrubber_categories.length; i++) {
-        rect = scrubber_categories[i].getBoundingClientRect();
-        scrubber_breakpoints[i] = rect.bottom;
-    }
+  for(i = 0; i < scrubber_categories.length; i++) {
+      rect = scrubber_categories[i].getBoundingClientRect();
+      scrubber_breakpoints[i] = rect.bottom;
+  }
 
   // Watches for touch on sidebar scrubber, applies the 'active' class and triggers anchors in response
   $('#slider-vertical').on("touchmove", function(e){
@@ -115,37 +67,6 @@ ready = function() {
   $('#slider-vertical').on("touchstart", function(e){
     $("[id^=scrubber-category]").removeClass("active");
   });
-
-  // Build caches progress bar
-  var progress = null;
-  var percantage = '100%';
-  var statsInterval;
-  var getCacheStats = function () {
-    $.get( "/dataStore/stats.json", function( data ) {
-      if ( progress == null ) {
-        // Set interval to keep checking stats
-        statsInterval = setInterval(function(){getCacheStats();}, 10000);
-      }
-
-      if (typeof data == 'undefined' || data.progress == "Completed" ) {
-        // Stop checking if no build in progress
-        progress = "No cache building in porgress"
-        clearInterval(statsInterval);
-      } else {
-        // Update values if process is not completed
-        progress = data.progress;
-        percentage = data.progress;
-      }
-
-      $('#cacheStats').css('width', percentage);
-      $('#cacheStats').text(progress);
-    }).fail(function() {
-      console.error("Could not fetch cache building progress");
-      clearInterval(statsInterval);
-    });
-  }
-
-  getCacheStats();
 
 };
 
