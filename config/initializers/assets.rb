395e2413f906ddf980d29d1360ac5f164857e106
Obada Kadri <okadri@ucdavis.edu>
2014-12-10 11:25:57 -0800

    #68 Moved min-max icon to middle of the box

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 Rails.application.config.assets.precompile += %w( jquery.wayfinding.js floor.js redirect.js animate.js jquery.panzoom.js )
 Rails.application.config.assets.precompile += %w( map.js show.js index.js search.js about.js landing.js admin.js ) # actions
-Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.eot )
-Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.woff )
-Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.svg )
-Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.ttf )
+Rails.application.config.assets.precompile += %w( bootstrap/glyphicons-halflings-regular.eot )
+Rails.application.config.assets.precompile += %w( bootstrap/glyphicons-halflings-regular.woff )
+Rails.application.config.assets.precompile += %w( bootstrap/glyphicons-halflings-regular.svg )
+Rails.application.config.assets.precompile += %w( bootstrap/glyphicons-halflings-regular.ttf )
 Rails.application.config.assets.precompile += %w( fontello.eot fontello.woff fontello.ttf )
