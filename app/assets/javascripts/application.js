4f3e20fe9cf433307b47e2a0e01414411cdded9e
Obada Kadri <okadri@ucdavis.edu>
2014-12-10 10:06:56 -0800

    #66 Possible fix for left nav bug in guided mode

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+
+var a=$('ul.vertical-nav a');
+for(var i=0;i<a.length;i++)
+{
+    a[i].onclick=function()
+    {
+        window.location=this.getAttribute("href");
+        return false
+    }
+}
