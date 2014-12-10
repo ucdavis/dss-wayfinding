9564dc9fc94cf2ad59a52890b3c3ee85a5e590bf
Obada Kadri <okadri@ucdavis.edu>
2014-12-10 10:16:24 -0800

    #66

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,12 +2,17 @@
 //= require jquery_ujs
 //= require bootstrap
 
-var a=$('ul.vertical-nav a');
-for(var i=0;i<a.length;i++)
-{
+$(function() {
+
+  // Handle left nav links using javascript (fixes guided mode issue on iPad)
+  var a=$('ul.vertical-nav a');
+
+  for(var i=0;i<a.length;i++)
+  {
     a[i].onclick=function()
     {
-        window.location=this.getAttribute("href");
-        return false
+      window.location=this.getAttribute("href");
+      return false;
     }
-}
+  }
+});
