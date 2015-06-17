48c8ccad39c218d6ebb38868a5874bf31eb0b787
Christopher Thielen <cmthielen@ucdavis.edu>
2015-06-17 10:34:52 -0700

    Disable broken RSS feed fetching

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,10 @@
 #set :output, "/path/to/my/cron_log.log"
 job_type :envcommand, 'cd :path && RAILS_ENV=:environment :task'
 
-# Ensure the local events DB is up-to-date
-every 15.minutes do
-  rake "rss:fetch"
-end
+# # Ensure the local events DB is up-to-date
+# every 15.minutes do
+#   rake "rss:fetch"
+# end
 
 # Ensure our background processor starts up on reboot
 every :reboot do
