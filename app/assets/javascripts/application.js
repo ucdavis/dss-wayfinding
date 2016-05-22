5ac3d5235465bb855ca6a8cd33f4a69cbe448f98
Steven Mackey <stevestser94@gmail.com>
2016-05-22 12:58:12 -0700

    removed jquery mobile stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,6 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require underscore
-//= require jquery.mobile-1.4.5.min.js
 
 var handleLinksWithJS = function () {
   // Handle left nav links using javascript (fixes guided mode issue on iPad)
