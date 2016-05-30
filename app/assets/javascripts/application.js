c3044212da19c025881776556a0fd871a6fd0b67
Steven Mackey <stevestser94@gmail.com>
2016-05-30 16:52:22 -0700

    works on iPod, not iPad

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,12 @@ $(function() {
   handleLinksWithJS();
   document.csrf = $('meta[name="csrf-token"]')[0].content;
 });
+
+// Add compatibility classes to required elements if user agent is determined to be mobile safari
+$(document).ready(function() {
+  if ((navigator.userAgent.match(/(iPod|iPhone)/) != null && navigator.userAgent.match(/AppleWebKit/) != null) || 1) {
+    $("#accessibility-nav").addClass("safari-mobile-compatibility");
+    $("body").addClass("safari-mobile-compatibility");
+    $("#map").addClass("safari-mobile-compatibility");
+  }
+});
