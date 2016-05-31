e437c8490d24e7b88f7ad00c8e0d038038ef8bc8
Steven Mackey <stevestser94@gmail.com>
2016-05-30 18:09:50 -0700

    add safari mobile compatibility to landing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,9 +25,9 @@ $(function() {
 // Add compatibility classes to required elements if user agent is determined to be mobile safari
 $(document).ready(function() {
   if ( (navigator.userAgent.match(/(iPod|iPhone)/) != null) && (navigator.userAgent.match(/AppleWebKit/) != null) ) {
-    console.log(navigator.userAgent.match(/(iPod|iPhone)/));
     $("#accessibility-nav").addClass("safari-mobile-compatibility");
     $("body").addClass("safari-mobile-compatibility");
     $("#map").addClass("safari-mobile-compatibility");
+    $(".landing-ul").addClass("safari-mobile-compatibility");
   }
 });
