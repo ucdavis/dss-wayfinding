7178af4d5cd5c4cec5e2bdf0649bf5c03529c424
Mark Diez <markediez@gmail.com>
2016-08-05 14:08:58 -0700

    Set map date as the last modified file in maps

--- config/initializers/map.rb
+++ config/initializers/map.rb
@@ -1,1 +1,6 @@
-$MAP_DATE = 0
+timestamp = []
+Dir["#{Rails.root.join('public', 'maps', '*.svg')}"].each do |file|
+  timestamp << File.mtime(file)
+end
+
+$MAP_DATE = timestamp ? timestamp.max.to_i : 0
