14e8c0bb7648dbce952b86188ee89113fe15ceba
Christopher Thielen <cmthielen@ucdavis.edu>
2017-06-13 10:42:13 -0700

    Update Capistrano set :scm syntax

--- Capfile
+++ Capfile
@@ -26,5 +26,8 @@ require 'capistrano/npm'
 
 require 'whenever/capistrano'
 
+require "capistrano/scm/git"
+install_plugin Capistrano::SCM::Git
+
 # Load custom tasks from `lib/capistrano/tasks' if you have any defined
 Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -12,9 +12,6 @@ set :passenger_restart_with_touch, true
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/deployer/apps/#{fetch(:application)}"
 
-# Default value for :scm is :git
-set :scm, :git
-
 # Default value for :format is :pretty
 set :format, :pretty
 
